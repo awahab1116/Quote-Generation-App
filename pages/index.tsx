b0f0acd0c2c9859cdfba80c8094bf2c8fc3c77d0
Awahab1116 <66561908+awahab1116@users.noreply.github.com>
2023-05-29 21:41:58 +0500

    add: project setup

---
+++ pages/index.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+
+
+// interface for our appsync <> lambda JSON response
+interface GenerateAQuoteData {
+  generateAQuote: {
+    statusCode: number;
+    headers: { [key: string]: string };
+    body: string;
+  }
+}
+
+
+// interface for our DynamoDB object
+interface UpdateQuoteInfoData {
+  id: string;
+  queryName: string;
+  quotesGenerated: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+
+
+
+export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+  const [openGenerator, setOpenGenerator] = useState(false);
+  const [processingQuote, setProcessingQuote] = useState(false);
+  const [quoteReceived, setQuoteReceived] = useState<String | null>(null);
+
+
+
+
+
+
+  // Functions for quote generator modal
+  const handleCloseGenerator = () => {
+    setOpenGenerator(false);
+    setProcessingQuote(false);
+    setQuoteReceived(null);
+  }
+
+
+  return (
+    <>
+      <Head>
+        <title>Inspirational Quote Generator</title>
+        <meta name="description" content="A fun project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
